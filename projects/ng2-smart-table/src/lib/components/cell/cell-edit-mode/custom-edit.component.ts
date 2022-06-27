eed8f578d367f5cf99583ace69dfa6ae835e6f5c
marmosco <marco.moscone@eng.it>
2022-06-27 12:04:04 +0200

    fix customEditing for custom editors

--- projects/ng2-smart-table/src/lib/components/cell/cell-edit-mode/custom-edit.component.ts
+++ projects/ng2-smart-table/src/lib/components/cell/cell-edit-mode/custom-edit.component.ts
@@ -12,14 +12,15 @@ import { EditCellDefault } from './edit-cell-default';
 
 @Component({
   selector: 'table-cell-custom-editor',
-  template: `
-    <ng-template #dynamicTarget></ng-template>
-  `,
+  template: ` <ng-template #dynamicTarget></ng-template> `,
 })
-export class CustomEditComponent extends EditCellDefault implements OnChanges, OnDestroy {
-
+export class CustomEditComponent
+  extends EditCellDefault
+  implements OnChanges, OnDestroy
+{
   customComponent: any;
-  @ViewChild('dynamicTarget', { read: ViewContainerRef, static: true }) dynamicTarget: any;
+  @ViewChild('dynamicTarget', { read: ViewContainerRef, static: true })
+  dynamicTarget: any;
 
   constructor(private resolver: ComponentFactoryResolver) {
     super();
@@ -27,15 +28,25 @@ export class CustomEditComponent extends EditCellDefault implements OnChanges, O
 
   ngOnChanges(changes: SimpleChanges) {
     if (this.cell && !this.customComponent) {
-      const componentFactory = this.resolver.resolveComponentFactory(this.cell.getColumn().editor.component);
-      this.customComponent = this.dynamicTarget.createComponent(componentFactory);
+      const componentFactory = this.resolver.resolveComponentFactory(
+        this.cell.getColumn().editor.component
+      );
+      this.customComponent =
+        this.dynamicTarget.createComponent(componentFactory);
 
       // set @Inputs and @Outputs of custom component
       this.customComponent.instance.cell = this.cell;
       this.customComponent.instance.inputClass = this.inputClass;
-      this.customComponent.instance.onStopEditing.subscribe(() => this.onStopEditing());
-      this.customComponent.instance.onEdited.subscribe((event: any) => this.onEdited(event));
-      this.customComponent.instance.onClick.subscribe((event: any) => this.onClick(event));
+      this.customComponent.instance.isCustomEditing = this.isCustomEditing;
+      this.customComponent.instance.onStopEditing.subscribe(() =>
+        this.onStopEditing()
+      );
+      this.customComponent.instance.onEdited.subscribe((event: any) =>
+        this.onEdited(event)
+      );
+      this.customComponent.instance.onClick.subscribe((event: any) =>
+        this.onClick(event)
+      );
     }
   }
 
